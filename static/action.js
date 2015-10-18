20ce071823aed89b207e859f4b69b126bca46f1b
Ferenc Béres <ferdzso05@gmail.com>
2015-10-18 19:00:28 +0200

    homepage style almost ready

--- static/action.js
+++ static/action.js
@@ -6,9 +6,65 @@ $(document).ready( function() {
     columnWidth: 160
   });
 
-  $grid.on( 'click', '.grid-item', function() {
+  $grid.on( 'click', '.sztaki', function() {
     // change size of item via class
-    $( this ).toggleClass('grid-item--gigante');
+    $( this ).toggleClass('grid-item--gigante sztaki--gigante');
+    // trigger layout
+    $grid.masonry();
+  });
+
+  $grid.on( 'click', '.msc', function() {
+    // change size of item via class
+    $( this ).toggleClass('grid-item--gigante msc--gigante');
+    // trigger layout
+    $grid.masonry();
+  });
+
+  $grid.on( 'click', '.bsc', function() {
+    // change size of item via class
+    $( this ).toggleClass('grid-item--gigante bsc--gigante');
+    // trigger layout
+    $grid.masonry();
+  });
+
+  $grid.on( 'click', '.praudit', function() {
+    // change size of item via class
+    $( this ).toggleClass('grid-item--gigante praudit--gigante');
+    // trigger layout
+    $grid.masonry();
+  });
+
+  $grid.on( 'click', '.bme', function() {
+    // change size of item via class
+    $( this ).toggleClass('grid-item--gigante bme--gigante');
+    // trigger layout
+    $grid.masonry();
+  });
+
+  $grid.on( 'click', '.ejg', function() {
+    // change size of item via class
+    $( this ).toggleClass('grid-item--gigante ejg--gigante');
+    // trigger layout
+    $grid.masonry();
+  });
+
+  $grid.on( 'click', '.bsi', function() {
+    // change size of item via class
+    $( this ).toggleClass('grid-item--gigante bsi--gigante');
+    // trigger layout
+    $grid.masonry();
+  });
+
+  $grid.on( 'click', '.prog_lang', function() {
+    // change size of item via class
+    $( this ).toggleClass('grid-item--gigante prog_lang--gigante');
+    // trigger layout
+    $grid.masonry();
+  });
+
+  $grid.on( 'click', '.distrib', function() {
+    // change size of item via class
+    $( this ).toggleClass('grid-item--gigante distrib--gigante');
     // trigger layout
     $grid.masonry();
   });
