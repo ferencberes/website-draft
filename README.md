bef78c888d6641989cd1063436beae56a6ffbb77
Ferenc Béres <ferdzso05@gmail.com>
2017-06-09 10:57:42 +0200

    myth npm package is not needed anymore for css variables

---
+++ README.md
@@ -0,0 +1,12 @@
+website-draft
+===========
+
+# Requirements
+
+## Install Hugo
+
+```bash
+sudo apt-get install hugo
+```
+
+
--- build_hugo.sh
+++ build_hugo.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-myth static/css/home_src.css static/css/home.css
-myth static/css/custom_src.css static/css/custom.css
 hugo server --buildDrafts
